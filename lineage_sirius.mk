9af8e9b148d86e86f59e2eb44b241ae611e07453
Alex Damaratski <alexeydomoratsky1@gmail.com>
2020-08-19 12:42:10 +0300

    Revert "sirius: Update build fingerprint to coral april"
    This reverts commit 500a895adbe372f00875d0cb1620faaec713da2a.

--- lineage_sirius.mk
+++ lineage_sirius.mk
@@ -16,13 +16,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8 SE
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/sirius/sirius:8.1.0/OPM1.171019.019/V9.5.6.0.OEBCNFA:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sirius-user 8.1.0 OPM1.171019.019 V9.5.6.0.OEBCNFA release-keys" \
     PRODUCT_NAME="sirius"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
